3f9bf0efcabf383897e877aeae93bca358527232
Dana Levy <danalev@wix.com>
2023-07-12 13:17:04 +0300

    cards

---
+++ src/_codux/boards/cards/cards.board.tsx
@@ -0,0 +1,13 @@
+import { createBoard } from '@wixc3/react-board';
+import { Cards } from '../../../components/cards/cards';
+
+export default createBoard({
+    name: 'Cards',
+    Board: () => <Cards />,
+    environmentProps: {
+        canvasBackgroundColor: '#ffffff',
+        windowBackgroundColor: '#ffffff',
+        windowWidth: 1520,
+        windowHeight: 1000,
+    },
+});
---
+++ src/components/cards/cards.tsx
@@ -0,0 +1,68 @@
+import styles from './cards.module.scss';
+import classNames from 'classnames';
+
+export interface CardsProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const Cards = ({ className }: CardsProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <div className={styles.container}>
+                <div className={styles.imagbox}>
+                    <img
+                        src="https://cdn.dribbble.com/users/4335179/screenshots/16490525/media/5dea3bbcf3939ddd6f5351a8efc493a2.jpg"
+                        alt=""
+                        className={styles.imag}
+                    />
+                </div>
+                <div>
+                    <h1>Happy Deer </h1>
+                    <p>
+                        The Greater Yellowstone Ecosystem is home to both mule deer and white-tailed
+                        deer. The two deer species are differentiated by their antler shape, and
+                        tail size and appearance.
+                    </p>
+                </div>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.imagbox}>
+                    <img
+                        src="https://cdn.dribbble.com/users/4335179/screenshots/16490525/media/5dea3bbcf3939ddd6f5351a8efc493a2.jpg"
+                        alt=""
+                        className={styles.imag}
+                    />
+                </div>
+                <div>
+                    <h1>Happy Deer </h1>
+                    <p>
+                        The Greater Yellowstone Ecosystem is home to both mule deer and white-tailed
+                        deer. The two deer species are differentiated by their antler shape, and
+                        tail size and appearance.
+                    </p>
+                </div>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.imagbox}>
+                    <img
+                        src="https://cdn.dribbble.com/users/4335179/screenshots/16490525/media/5dea3bbcf3939ddd6f5351a8efc493a2.jpg"
+                        alt=""
+                        className={styles.imag}
+                    />
+                </div>
+                <div>
+                    <h1>Happy Deer </h1>
+                    <p>
+                        The Greater Yellowstone Ecosystem is home to both mule deer and white-tailed
+                        deer. The two deer species are differentiated by their antler shape, and
+                        tail size and appearance.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
