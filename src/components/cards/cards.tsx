da4b6864c55e8b5ff2812494153a34b3f76a66a9
Dana Levy <danalev@wix.com>
2023-08-01 09:39:45 +0300

    imagurl

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -3,14 +3,16 @@ import classNames from 'classnames';
 import { TypeIcon } from '../type-icon/type-icon';
 
 export interface CardsProps {
+    imageUrl: string;
     className?: string;
+
 }
 
 /**
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
-export const Cards = ({ className }: CardsProps) => {
+export const Cards = ({ className, imageUrl }: CardsProps) => {
     return (
         <div className={classNames(styles.root, className)}>
             <div>
@@ -22,19 +24,16 @@ export const Cards = ({ className }: CardsProps) => {
                         <h1 className={styles.title}>Eevee</h1>
                         <div className={styles.header}>
                             <p className={styles.pawer}>50</p>
-                            <TypeIcon className={styles.icon} />
+                            <TypeIcon className={styles.icon} energyType="colorless" />
                         </div>
                     </div>
-                    <div className={styles.imgbox}>
-                        <img
-                            src="https://files.cults3d.com/uploaders/16165612/illustration-file/d19b7a5e-2e51-40bf-baa6-d25e8bd9ef9f/Eevee_Stand02.jpg"
-                            alt=""
-                            className={styles.img}
-                        />
-                    </div>
+                    <div className={styles.imgbox} style={{
+                        backgroundImage: `url('${imageUrl}')`
+                        }}
+                        ></div>
                     <div>
                         <div className={styles.flex}>
-                            <TypeIcon />
+                            <TypeIcon energyType="colorless" />
                             <h3>Be Prepared</h3>
                             <h3>12</h3>
                         </div>
@@ -45,9 +44,9 @@ export const Cards = ({ className }: CardsProps) => {
                     <div>
                         <div className={styles.flex}>
                             <div className={styles.flexicons}>
-                                <TypeIcon />
-                                <TypeIcon />
-                                <TypeIcon />
+                                <TypeIcon energyType="colorless" />
+                                <TypeIcon energyType="colorless" />
+                                <TypeIcon energyType="colorless" />
                             </div>
                             <h3>Be Prepared</h3>
                             <h3>12</h3>
