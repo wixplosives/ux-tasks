fd0c6a62cf0bd2cb074b9db6777814e7018ee83b
Dana Levy <danalev@wix.com>
2023-08-06 14:28:49 +0300

    sunday

--- src/_codux/boards/energy-type/energy-types.board.tsx
+++ src/_codux/boards/energy-type/energy-types.board.tsx
@@ -4,5 +4,5 @@ import { EnergyType } from '../../../components/energy-type/energy-type';
 export default createBoard({
     name: 'Energy Type',
 
-    Board: () => <EnergyType energyType="psychic" />,
+    Board: () => <EnergyType energyType="darkness" />,
 });
