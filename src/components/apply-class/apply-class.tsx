7f0af8ad44bd2a26fa555021051e64d2441511d0
Dana Levy <danalev@wix.com>
2023-08-10 09:47:40 +0300

    add apply class component

--- src/components/apply-class/apply-class.tsx
+++ src/components/apply-class/apply-class.tsx
@@ -10,5 +10,41 @@ export interface ApplyClassProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const ApplyClass = ({ className }: ApplyClassProps) => {
-    return <div className={classNames(styles.root, className)}>ApplyClass</div>;
+    return (
+        <div className={classNames(styles.root, className)}>
+            <p>Black</p>
+            <p>LightSlateGrey</p>
+
+            <p>Dark Gray</p>
+            <p>LightBlue</p>
+            <p>LightSkyBlue</p>
+            <p>Blue</p>
+            <p>DarkSlateBlue</p>
+            <p>Violet</p>
+            <p>MediumPurple</p>
+            <p>MediumOrchid</p>
+            <p>HotPink</p>
+            <p>LightPink</p>
+            <p>FireBrick</p>
+            <p>Red</p>
+            <p>OrangeRed</p>
+            <p>Orange</p>
+            <p>Tomato</p>
+            <p>Coral</p>
+            <p>Sienna</p>
+            <p>IndianRed</p>
+            <p>PaleVioletRed</p>
+            <p>Pink</p>
+            <p>Purple</p>
+            <p>DarkSlateGray</p>
+            <p>SeaGreen</p>
+            <p>MediumAquaMarine</p>
+            <p>Turquoise</p>
+            <p>DarkTurquoise</p>
+            <p>LightSeaGreen</p>
+            <p>Teal</p>
+            <p>Olive</p>
+            <p>Khaki</p>
+        </div>
+    );
 };
