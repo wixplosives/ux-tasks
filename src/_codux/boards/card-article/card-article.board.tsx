d0bc1ee8a0dbc7a49268fe5642db42986c94bbcb
Codux <codux@codux.com>
2024-01-30 16:02:17 +0200

    responsive text

--- src/_codux/boards/card-article/card-article.board.tsx
+++ src/_codux/boards/card-article/card-article.board.tsx
@@ -1,11 +1,13 @@
 import { createBoard } from '@wixc3/react-board';
 import { CardArticle } from '../../../components/card-article/card-article';
+import CardArticle_module from '../../../components/card-article/card-article.module.scss';
 
 export default createBoard({
     name: 'CardArticle',
-    Board: () => <CardArticle />,
+    Board: () => <CardArticle className={CardArticle_module.textBox} />,
     isSnippet: true,
     environmentProps: {
-        windowWidth: 630,
+        windowWidth: 760,
+        canvasHeight: 332,
     },
 });
