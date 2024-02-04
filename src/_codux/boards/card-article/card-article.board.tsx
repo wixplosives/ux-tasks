17ecfa320772e07fd09b14d81d49d2c562c1510a
Codux <codux@codux.com>
2024-02-04 14:19:10 +0200

    change size

--- src/_codux/boards/card-article/card-article.board.tsx
+++ src/_codux/boards/card-article/card-article.board.tsx
@@ -7,8 +7,15 @@ export default createBoard({
     Board: () => <CardArticle className={CardArticle_module.textBox} />,
     isSnippet: true,
     environmentProps: {
-        windowWidth: 596,
+        windowWidth: 784,
         canvasHeight: 332,
         windowHeight: 1024,
+        canvasWidth: 710,
+        canvasMargin: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+        },
     },
 });
