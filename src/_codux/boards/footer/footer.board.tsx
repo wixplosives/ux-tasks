8f9db7c85adcc24806fe9d2c4fad905ba5daabe5
alisaLachman <alisal@wix.com>
2024-02-05 15:54:11 +0200

    delete boards, keep card

--- src/_codux/boards/footer/footer.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Footer } from '../../../components/footer/footer';
-
-export default createBoard({
-    name: 'Footer',
-    Board: () => <Footer />
-});
--- src/_codux/boards/footerline/footerline.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Footerline } from '../../../components/footerline/footerline';
-
-export default createBoard({
-    name: 'Fotterline',
-    Board: () => <Footerline />,
-});
